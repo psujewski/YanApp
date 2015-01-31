ded0ffe5993fa832bf8e47a68833933cd07e6ed3
lradzikowski <lradzikowski@sylogic.pl>
2015-01-31 17:51:29 +0100

    Update dropbox-js version number

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ then
 fi
 
 wget "http://code.jquery.com/jquery-2.1.3.min.js" -O "${LIB_PATH}/jquery.min.js"
-wget "https://cdnjs.cloudflare.com/ajax/libs/dropbox.js/0.10.2/dropbox.min.js" -O "${LIB_PATH}/dropbox.min.js"
-wget "https://cdnjs.cloudflare.com/ajax/libs/dropbox.js/0.10.2/dropbox.min.map" -O "${LIB_PATH}/dropbox.min.map"
-wget "https://cdnjs.cloudflare.com/ajax/libs/dropbox.js/0.10.2/dropbox.js" -O "${LIB_PATH}/dropbox.js"
+wget "https://cdnjs.cloudflare.com/ajax/libs/dropbox.js/0.10.3/dropbox.min.js" -O "${LIB_PATH}/dropbox.min.js"
+wget "https://cdnjs.cloudflare.com/ajax/libs/dropbox.js/0.10.3/dropbox.min.map" -O "${LIB_PATH}/dropbox.min.map"
+wget "https://cdnjs.cloudflare.com/ajax/libs/dropbox.js/0.10.3/dropbox.js" -O "${LIB_PATH}/dropbox.js"
